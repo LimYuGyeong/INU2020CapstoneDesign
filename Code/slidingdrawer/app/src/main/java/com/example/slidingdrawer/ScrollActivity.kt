1747f74f4ac7e733720dc09fa86a507c6ba79b8d
LimYuGyeong <profiler0916@gmail.com>
2021-03-08 17:05:55 +0900

    large folder test

---
+++ Code/slidingdrawer/app/src/main/java/com/example/slidingdrawer/ScrollActivity.kt
@@ -0,0 +1,12 @@
+package com.example.slidingdrawer
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ScrollActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scroll_main)
+    }
+}
