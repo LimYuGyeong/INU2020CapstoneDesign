69d42528259ba5f5e91906fdabe9c142d5732e24
LimYuGyeong <profiler0916@gmail.com>
2021-03-27 17:38:26 +0900

    알람 기능 추가. alarm.js만 수정함

--- OhMyUmbrella/js/alarm.js
+++ OhMyUmbrella/js/alarm.js
@@ -41,4 +41,35 @@ function set_time(element){
 	localStorage.setItem("set_time",set_time);
 	var element = document.getElementById("now_set_time");
 	element.innerText = set_time;
+	
+	// 알람설정 부분 //
+	if (set_time) {
+ 		var time = set_time.split(':');
+ 	 	var hour = time[0];
+ 	 	var minute = time[1];
+ 	 	
+ 	 	hour = parseInt(hour);
+ 	 	minute = parseInt(minute);
+// 	 	console.log("hour : " , hour, typeof(hour))
+// 		console.log("minute : " , minute, typeof(minute))
+ 		
+ 	 	var now = new Date();
+ 		
+ 		var notification = new tizen.UserNotification('SIMPLE', 'Simple notification', notificationGroupDict);
+ 		var date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute);
+ 		var alarm = new tizen.AlarmAbsolute(date);
+
+ 		var notificationGroupDict =
+ 		{
+ 		  content: "오늘은 비가 오니 우산을 챙겨가세요!",
+ 		  actions: {soundPath: "music/Over the horizon.mp3", vibration: true}
+ 		};
+ 		
+ 		var notification = new tizen.UserNotification("SIMPLE", "오늘의 우산", notificationGroupDict);
+ 		
+ 		/* Adds an alarm. */
+ 		tizen.alarm.addAlarmNotification(alarm, notification);
+ 		
+ 		}
+		
 }
