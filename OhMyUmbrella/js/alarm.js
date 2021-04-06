9a7b24c447147ad465e41a2b766870554c06b8ca
LimYuGyeong <70835175+LimYuGyeong@users.noreply.github.com>
2021-04-06 23:55:21 +0900

    Update alarm.js : 알람 on-off 기능 추가 , 코드 다듬음

--- OhMyUmbrella/js/alarm.js
+++ OhMyUmbrella/js/alarm.js
@@ -27,9 +27,20 @@ function toggle(element){
 	var onoff = document.getElementById("onoff")
 	if (onoff.innerText == "알림 off"){
 		onoff.innerText = "알림 on";
+		
+		
+		/* 알람 on 됐으니 재설정 */
+		var hour = localStorage.getItem('HOUR');
+		var minute = localStorage.getItem('MINUTE');
+		
+		add_alarm(hour, minute);
 	}
+	
 	else{
 		onoff.innerText = "알림 off";
+		
+		var alarms = tizen.alarm.getAll();
+		tizen.alarm.removeAll(); // 설정된 알람 모두 삭제
 	}
 		
 }
@@ -37,39 +48,45 @@ function toggle(element){
 
 function set_time(element){
 	
+	/* 설정한 시각으로 텍스트를 바꾸는 부분 */
 	var set_time = document.getElementById("time").value;
 	localStorage.setItem("set_time",set_time);
 	var element = document.getElementById("now_set_time");
 	element.innerText = set_time;
 	
-	// 알람설정 부분 //
-	if (set_time) {
+	/* 알람설정 부분 */
+	if (set_time) { 
  		var time = set_time.split(':');
  	 	var hour = time[0];
  	 	var minute = time[1];
  	 	
  	 	hour = parseInt(hour);
  	 	minute = parseInt(minute);
-// 	 	console.log("hour : " , hour, typeof(hour))
-// 		console.log("minute : " , minute, typeof(minute))
+ 	 	localStorage.setItem("HOUR", hour);
+ 	 	localStorage.setItem("MINUTE", minute);
  		
- 	 	var now = new Date();
- 		
- 		var notification = new tizen.UserNotification('SIMPLE', 'Simple notification', notificationGroupDict);
- 		var date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute);
- 		var alarm = new tizen.AlarmAbsolute(date);
-
- 		var notificationGroupDict =
- 		{
- 		  content: "오늘은 비가 오니 우산을 챙겨가세요!",
- 		  actions: {soundPath: "music/Over the horizon.mp3", vibration: true}
- 		};
- 		
- 		var notification = new tizen.UserNotification("SIMPLE", "오늘의 우산", notificationGroupDict);
- 		
- 		/* Adds an alarm. */
- 		tizen.alarm.addAlarmNotification(alarm, notification);
- 		
- 		}
+		add_alarm(hour, minute);
+ 	 	
+ 	}
 		
+}
+
+
+function add_alarm(hour, minute){
+	
+	var now = new Date();
+	var date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute);
+	var alarm = new tizen.AlarmAbsolute(date, ["MO", "TU", "WE", "TH", "FR", "SA", "SU"]); // date 시각에 첫 알람이 시작해서 매일 반복
+
+	var notificationGroupDict =
+	{
+	  content: "오늘은 비가 오니 우산을 챙겨가세요!",
+	  actions: {soundPath: "music/Over the horizon.mp3", vibration: true}
+	};
+	
+	var notification = new tizen.UserNotification("SIMPLE", "오늘의 우산", notificationGroupDict);
+	
+	/* Adds an alarm. */
+	tizen.alarm.addAlarmNotification(alarm, notification);
+	
 }
