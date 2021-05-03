d970f92a6b28872f61cfae10c40cc6fb6ed9ea7c
jawswhite95 <48754674+jawswhite95@users.noreply.github.com>
2021-05-03 21:07:16 +0900

    날씨 아이콘 추가 javascript

--- OhMyUmbrella/js/main.js
+++ OhMyUmbrella/js/main.js
@@ -20,20 +20,40 @@ window.onload = function () {
 };
  
 
+localStorage.setItem("c_or_d","c");
+var c_or_d = localStorage.getItem("c_or_d");
+console.log(c_or_d);
 /* index.html 화면이 열릴때 마다 실행되는 함수로
  * 상단의 날씨 이미지를 갱신하는 기능
  *  */
 function main_init(){
-	
-	updateWeather();
+	var c_or_d = localStorage.getItem("c_or_d");
+	console.log("main_init 안");
+	console.log(c_or_d);
+	if(c_or_d == "c"){
+		updateWeatherCurrentLocation();
+	}
+	else if(c_or_d == "d"){
+		updateWeatherDestination();
+	}
 	
 	setTimeout(set_img, 1000);
 	
 	function set_img(){
-		var POP = localStorage.getItem("POP");
-		var PTY = localStorage.getItem("PTY");
-		var SKY = localStorage.getItem("SKY");
-		var T3H = localStorage.getItem("T3H");
+	
+		if(c_or_d == "c"){
+			var POP = localStorage.getItem("c_POP");
+			var PTY = localStorage.getItem("c_PTY");
+			var SKY = localStorage.getItem("c_SKY");
+			var T3H = localStorage.getItem("c_T3H");
+		}
+		else if(c_or_d == "d"){
+			var POP = localStorage.getItem("POP");
+			var PTY = localStorage.getItem("PTY");
+			var SKY = localStorage.getItem("SKY");
+			var T3H = localStorage.getItem("T3H");
+		}
+		
 		if (POP && PTY && SKY && T3H) {
 			//var POP = 0;
 			//var PTY = 0;
@@ -68,6 +88,14 @@ function main_init(){
 			
 				
 			// T3H 3시간 기온
+			var CorD = document.getElementById("CorD");
+			if (c_or_d == "c"){
+				CorD.innerText = "현위치";
+			} 
+			else if (c_or_d == "d"){
+				CorD.innerText = "목적지";
+			}
+			
 			var Temperatures = document.getElementById("Temperatures");
 			Temperatures.innerText = "기온:"+ T3H + "°C";
 			// POP 강수확률 #알림은 POP기준
@@ -75,7 +103,16 @@ function main_init(){
 			probability.innerText = "강수 확률:"+ POP + "%";
 		
 			}
-			else{
+		else{
+				console.log("여기라도..");
+				var CorD = document.getElementById("CorD");
+				if (c_or_d == "c"){
+					CorD.innerText = "현위치";
+				} 
+				else if (c_or_d == "d"){
+					CorD.innerText = "목적지";
+				}
+				
 				var weather_img = document.getElementById("weather_img");
 				weather_img.src = "/image/weather_icon/null.png";
 				// T3H 3시간 기온
@@ -87,17 +124,30 @@ function main_init(){
 			}
 	}
 }
-	
 
+function chageLocation(){
+	var c_or_d = localStorage.getItem("c_or_d");
+	if(c_or_d == "c"){
+		localStorage.setItem("c_or_d","d");
+	}
+	else{
+		localStorage.setItem("c_or_d","c");
+	}
+	main_init();
+}
 
-
-function updateWeather(){
-	   /* 현재 시간을 매개변수로 하여 날씨정보를 불러올 시간을 반환하는 함수 */
-	   function getFormatTime(hour) {
+function updateWeatherCurrentLocation(){
+	/* 현재 시간을 매개변수로 하여 날씨정보를 불러올 시간을 반환하는 함수 */
+	   function getFormatTime(date) {
+		   var hour = date.getHours();
+		   var day = date.getDate();
+		   
 	      if (hour < 3) {
 	         temp_hour = '20' 
+	        date.setDate(day-1);
 	      } else if (hour < 6) {
 	         temp_hour = '23' 
+        	 date.setDate(day-1);
 	      } else if (hour < 9) {
 	         temp_hour = '02' 
 	      } else if (hour < 12) {
@@ -126,9 +176,168 @@ function updateWeather(){
 
 	   var date = new Date();
 
-	   hour = date.getHours();                // 현재 시간
+	   //hour = date.getHours();                // 현재 시간
 
-	   var base_time = getFormatTime(hour)    // 날씨정보를 불러올 시간
+	   var base_time = getFormatTime(date)    // 날씨정보를 불러올 시간
+	   //console.log(base_time);
+
+	   var base_date = getFormatDate(date);   // 날씨정보를 불러올 날짜
+	   console.log('base_date : ' + base_date);
+
+
+
+	   var Nx;
+	   var Ny;
+	      
+	      $.ajax({
+	          url: '../excel/gisang_real_final_utf.csv',
+	          dataType: 'text',
+	          //contentType: "application/x-www-form-urlencoded; charset=euc-kr"
+	        }).done(successFunction);
+
+	      function successFunction(data) {
+	          //console.log('success');
+
+	          var allRows = data.split(/\r?\n|\r/);
+	          
+	          for (var singleRow = 1; singleRow < allRows.length; singleRow++) {
+
+	             if(allRows[singleRow].includes(localStorage.getItem("c_SEL_DO")) && allRows[singleRow].includes(localStorage.getItem("c_SEL_SIGUNGU")))
+	             {
+	                var split_str = allRows[singleRow].split(',');
+	                Nx = split_str[3];
+	                Ny = split_str[4];
+	                //console.log('x : ' + Nx + 'y: ' + Ny);
+	             }
+	             
+	          }
+	        }
+
+
+	   /*---------------------------------------------------------------------------------------------*/
+
+
+
+
+
+	   setTimeout(send_url, 100);  // csv파일을 읽어 온 후 URL을 생성하기 위한 코드
+
+	   var pop;  // 강수확률
+	   var pty;  // 강수형태
+	   var sky;  // 하늘상태
+
+	   /* 서버에 json 형태의 최신날씨정보를 요청하고 날씨 정보를 로컬스토리지에 저장하는 함수*/
+	   function send_url(){
+
+	      var xhr = new XMLHttpRequest();
+	       
+	       var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst'; /*URL*/
+	       var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+'mlzKHRPrHbPpzQwhDvGPcvWIlwvBQ4Ovap5TRNxs3%2BlQrrCs7dFpbrDCfoLOTJxhD3RJA8l5GS%2B4meEDTtHSzA%3D%3D'; /*Service Key*/
+	       queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+	       queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
+	       queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('JSON'); /**/
+	       queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent(base_date); /**/
+	       queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent(base_time); /**/
+	       queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent(Nx); /**/
+	       queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent(Ny); /**/
+	       xhr.open('GET', url + queryParams);
+	       xhr.onreadystatechange = function () {
+	           if (this.readyState == 4) {
+	              //console.log('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+	              var weather_TEXT = this.responseText;
+	              //console.log(weather_TEXT);
+	              //console.log(typeof(weather_TEXT));
+	              var weather_JSON = JSON.parse(weather_TEXT);
+	              //console.log(weather_JSON);
+	              //console.log(typeof(weather_JSON));
+	              
+	              //console.log(base_date);
+	              //console.log(base_time);
+	              
+	              //console.log(Nx);
+	              //console.log(Ny);
+	              
+	              //console.log(weather_JSON.response.body.items.item[0].fcstValue);
+	              for (var i = 0; i < 10; i++) {
+	           	   if (weather_JSON.response.body.items.item[i].category == "POP"){  // 강수확률 정보를 가져옴
+	           		   pop = weather_JSON.response.body.items.item[i].fcstValue;
+	           	   }
+	           	   else if (weather_JSON.response.body.items.item[i].category == "PTY") {  // 강수형태 정보를 가져옴
+	           		   pty = weather_JSON.response.body.items.item[i].fcstValue;
+	   			   }
+	           	   else if (weather_JSON.response.body.items.item[i].category == "SKY") {  // 하늘상태 정보를 가져옴
+	           		   sky = weather_JSON.response.body.items.item[i].fcstValue;
+	   			   }
+	           	   else if (weather_JSON.response.body.items.item[i].category == "T3H") {  // 3시간 기온 정보를 가져옴
+	           		   t3h = weather_JSON.response.body.items.item[i].fcstValue;
+	   			   }
+	   		      }
+	              //console.log(pop);
+	              //console.log(pty);
+	              //console.log(sky);
+	              localStorage.setItem("c_POP",pop);
+	              localStorage.setItem("c_PTY",pty);
+	              localStorage.setItem("c_SKY",sky);
+	              localStorage.setItem("c_T3H",t3h);
+	              console.log("c_POP: "+localStorage.getItem("c_POP"));
+	              console.log("c_PTY: "+localStorage.getItem("c_PTY"));
+	              console.log("c_SKY: "+localStorage.getItem("c_SKY"));
+	              console.log("c_T3H: "+localStorage.getItem("c_T3H"));
+	              
+	            
+	              
+	           }
+	       };
+
+	       xhr.send('');
+	   }
+	}
+
+
+
+function updateWeatherDestination(){
+	   /* 현재 시간을 매개변수로 하여 날씨정보를 불러올 시간을 반환하는 함수 */
+	   function getFormatTime(date) {
+		   var hour = date.getHours();
+		   var day = date.getDate();
+		   
+	      if (hour < 3) {
+	         temp_hour = '20' 
+	        date.setDate(day-1);
+	      } else if (hour < 6) {
+	         temp_hour = '23' 
+        	 date.setDate(day-1);
+	      } else if (hour < 9) {
+	         temp_hour = '02' 
+	      } else if (hour < 12) {
+	         temp_hour = '05' 
+	      } else if (hour < 15) {
+	         temp_hour = '08' 
+	      } else if (hour < 18) {
+	         temp_hour = '11' 
+	      } else if (hour < 21) {
+	         temp_hour = '14' 
+	      } else if (hour < 24) {
+	         temp_hour = '17' 
+	      }
+	      return temp_hour + '00'    
+	   }
+
+	   /* 현재 날짜를 가져오는 함수를 매개변수로 하여 날씨정보를 불러올 날짜를 반환하는 함수 */
+	   function getFormatDate(date){
+	       var year = date.getFullYear();              // yyyy
+	       var month = (1 + date.getMonth());          // M
+	       month = month >= 10 ? month : '0' + month;  // month 두자리로 저장
+	       var day = date.getDate();                   // d
+	       day = day >= 10 ? day : '0' + day;          // day 두자리로 저장
+	       return  year + '' + month + '' + day;       // yyyymmdd 형태로 반환
+	   }
+
+	   var date = new Date();
+
+	   //hour = date.getHours();                // 현재 시간
+
+	   var base_time = getFormatTime(date)    // 날씨정보를 불러올 시간
 	   //console.log(base_time);
 
 	   var base_date = getFormatDate(date);   // 날씨정보를 불러올 날짜
@@ -242,3 +451,43 @@ function updateWeather(){
 	       xhr.send('');
 	   }
 	}
+
+function re_connect() {
+	var address = localStorage.getItem("ADDRESS");
+	var adapter = tizen.bluetooth.getLEAdapter();
+	
+	var connectionListener = {
+		    onconnected: function(device) {
+		    	alert('Connected to the device: ' + device.name + ' [' + device.address + ']');
+		    },
+		    ondisconnected: function(device) {
+		    	adapter.startScan(onsuccess);
+		    	alert('Disconnected from the device ' + device.name + ' [' + device.address + ']');
+		    }
+		};
+
+	function connectFail(e) {
+	    alert('Failed to connect to device: ' + e.message);
+	}
+
+	function connectSuccess() {
+		localStorage.setItem("ADDRESS", address);
+		console.log('Connected to device');
+	}
+	
+	function onsuccess(device)
+	{
+		if (device.address == address)
+	  {
+	    console.log("Found device: " + device.name);
+	    adapter.stopScan();
+	    
+	    watchId = device.addConnectStateChangeListener(connectionListener);
+	    
+	    device.connect(connectSuccess, connectFail); //connect
+	  }
+	}
+	
+	adapter.startScan(onsuccess);
+	
+}
